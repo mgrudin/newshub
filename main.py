0de2e39e16a6aea82595c81966315547cd89f106
Maksym Hrudin <snowjotunn@gmail.com>
2026-06-05 16:13:11 +0100

    Add Jinja2 template of main page

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from sqlalchemy import select
+
+from app.articles import fetch_source
+from app.db import SessionLocal
+from app.models import Source
+
+templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
+sources = []
+
+with SessionLocal() as session:
+    sources = session.scalars(select(Source)).all()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="index.html", context={"sources": sources}
+    )
