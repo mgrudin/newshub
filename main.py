46ad106d65092ca2fab200a2bd862f14307f6906
Maksym Hrudin <snowjotunn@gmail.com>
2026-06-08 17:05:46 +0100

    Show articles from db

--- main.py
+++ main.py
@@ -3,21 +3,26 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy import select
 
-from app.articles import fetch_source
 from app.db import SessionLocal
-from app.models import Source
+from app.models import Article, Source
 
 templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 sources = []
+articles = []
 
 with SessionLocal() as session:
     sources = session.scalars(select(Source)).all()
+    articles = session.scalars(
+        select(Article).where(Article.source_id == Source.id)
+    ).all()
 
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
     return templates.TemplateResponse(
-        request=request, name="index.html", context={"sources": sources}
+        request=request,
+        name="index.html",
+        context={"sources": sources, "articles": articles},
     )
